4bd97d30461ca6766953b94d03a69ca05e46bcd0
Varun <2300033120@kluniversity.in>
2025-09-26 18:19:59 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://http://16.171.129.85:8080/auth";
+const API_URL = "http://54.234.177.120:8080/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
